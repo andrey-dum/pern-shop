96441992f7b00c1be90adb9d761aa7761e4c7883
andrey-dum <duminskiy@gmail.com>
2021-02-02 15:35:37 +0200

    controllers

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,17 @@
+const brandController = require("./brandController");
+
+class DeviceController {
+    async create(req, res) {
+
+    }
+
+    async getAll(req, res) {
+
+    }
+
+    async getOne(req, res) {
+
+    }
+}
+
+module.exports - new brandController()
