65ad6305b64e0b95ae72d9879531a4111a732722
andrey-dum <duminskiy@gmail.com>
2021-02-02 15:52:46 +0200

    ApiError, errorHandlingMiddleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 
@@ -12,9 +13,11 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({message: 'Working!!!'})
-// })
+
+// Обработка ошибок. Последний middleware
+app.use(errorHandler)
+
+
 
 const start = async () => {
     try {
