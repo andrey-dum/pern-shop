a9efe74c74febbe7fe192292ac8e4ff15045c4f7
andrey-dum <duminskiy@gmail.com>
2021-02-04 20:15:39 +0200

    Registration/Auth Page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
+
 
 export const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
-        <div>
-            <h1>Auth</h1>
-        </div>
+        <Container 
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-4"
+                        placeholder="Введите email..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите password..."
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                     {isLogin ? 
+                        <div>
+                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся</NavLink>
+                        </div>
+                        :
+                        <div>
+                            Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войти</NavLink>
+                        </div>
+                    }
+                
+                        <Button 
+                            variant="outline-success">
+                           { isLogin ? 'Войти' : 'Регистрация'}
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
     )
 }
